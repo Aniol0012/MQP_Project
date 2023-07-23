da04ec6fdc891a6b4099fb6112e763e012025495
Aniol0012 <aniol0012@gmail.com>
2023-07-23 12:56:22 +0200

    .

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 
-root = tk.Tk()
-root.geometry("800x600")  # Cambia el tamaño de la ventana a 800x600
 
-# Carga el logo y lo coloca en la esquina superior izquierda
-logo_image = Image.open("logo.png")
+def calculate():
+    num1 = float(entry1.get())
+    num2 = float(entry2.get())
+    result = num1 + num2
+    result_label.config(text=f"Resultado: {result}")
+
+
+root = tk.Tk()
+root.geometry("800x600")
+root.title("Calculadora de Suma")
+
+logo_image = Image.open("mqp.png")
 photo = ImageTk.PhotoImage(logo_image)
 logo_label = tk.Label(root, image=photo)
 logo_label.pack(anchor="nw")
 
-# Cambia el icono de la aplicación
-root.iconbitmap('icono.ico')
+root.iconbitmap('mqp.ico')  # https://convertio.co/es/png-ico/
+
+label1 = tk.Label(root, text="Número 1:")
+label1.pack()
+
+entry1 = tk.Entry(root)
+entry1.pack()
+
+label2 = tk.Label(root, text="Número 2:")
+label2.pack()
+
+entry2 = tk.Entry(root)
+entry2.pack()
+
+button = tk.Button(root, text="Calcular", command=calculate)
+button.pack()
+
+result_label = tk.Label(root, text="Resultado:")
+result_label.pack()
 
 root.mainloop()
