873d38edbf8a26b7346b53badd0689d73b6c282f
Aniol0012 <aniol0012@gmail.com>
2023-07-23 13:34:00 +0200

    Improved buttons and visuals

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 
+
 def calculate():
     try:
         num1 = float(entry1.get())
@@ -10,52 +11,58 @@ def calculate():
     except ValueError:
         update_result_label("Se han introducido valores incorrectos")
 
+
 def clear():
     entry1.delete(0, tk.END)
     entry2.delete(0, tk.END)
     update_result_label("")
 
+
 def update_result_label(new_text):
     result_label.config(text=f"Resultado: {new_text}")
 
+
+def center_window(root, width, height):
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    position_top = int(screen_height / 2 - height / 2)
+    position_right = int(screen_width / 2 - width / 2)
+    root.geometry(f"{width}x{height}+{position_right}+{position_top}")
+
+
 root = tk.Tk()
-root.geometry("800x600")
 root.title("Calculadora de Suma")
 root.configure(bg='light grey')
 
-# Centrar la ventana en la pantalla
-window_width = 800
-window_height = 600
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-position_top = int(screen_height / 2 - window_height / 2)
-position_right = int(screen_width / 2 - window_width / 2)
-root.geometry(f"{window_width}x{window_height}+{position_right}+{position_top}")
+center_window(root, 800, 600)
 
 logo_image = Image.open("mqp.png")
 photo = ImageTk.PhotoImage(logo_image)
 logo_label = tk.Label(root, image=photo, bg='light grey')
-logo_label.pack(anchor="nw")
+logo_label.pack(anchor="center")
 
 root.iconbitmap('mqp.ico')
 
-label1 = tk.Label(root, text="Número 1:", bg='light grey')
+label1 = tk.Label(root, text="Número 1:", bg='light grey', font=('Helvetica', '10', 'bold'))
 label1.pack()
 
-entry1 = tk.Entry(root)
+entry1 = tk.Entry(root, bd=2, width=25)
 entry1.pack()
 
-label2 = tk.Label(root, text="Número 2:", bg='light grey')
+label2 = tk.Label(root, text="Número 2:", bg='light grey', font=('Helvetica', '10', 'bold'))
 label2.pack()
 
-entry2 = tk.Entry(root)
+entry2 = tk.Entry(root, bd=2, width=25)
 entry2.pack()
 
-button = tk.Button(root, text="Calcular", command=calculate)
-button.pack()
+button_frame = tk.Frame(root, bg='light grey')
+button_frame.pack(pady=10)
 
-clear_button = tk.Button(root, text="Limpiar", command=clear)
-clear_button.pack()
+clear_button = tk.Button(button_frame, text="Limpiar", command=clear, bg='orange', height=2, width=10)
+clear_button.grid(row=0, column=0, padx=10)
+
+button = tk.Button(button_frame, text="Calcular", command=calculate, bg='green', height=2, width=10)
+button.grid(row=0, column=1)
 
 result_label = tk.Label(root, text="Resultado:", bg='light grey')
 result_label.pack()
