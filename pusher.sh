cc73b9e2524d8e5ab9ce1477a64ba12fd4769495
Aniol0012 <aniol0012@gmail.com>
2023-08-10 22:53:57 +0200

    v10.2.12

--- pusher.sh
+++ pusher.sh
@@ -14,6 +14,24 @@ git add .
 git commit -m "v$1"
 git push
 
+# Switch to the EN branch or create it if it doesn't exist
+git checkout -b EN || git checkout EN
+
+# Remove the files you don't want to copy to the EN branch
+git rm --cached config.py
+git rm --cached README.md
+
+# Copy all changes from main branch excluding what's in .gitignore
+git checkout main -- .
+
+# Commit and push changes to the EN branch
+git commit -m "v$1 - Copied from main"
+git push origin EN
+
+# Switch back to the main branch
+git checkout main
+
+# Restore the line in config.py
 sed -i 's/SCREEN_TO_OPEN_ROOT = 0  # 1 Para la segunda pantalla y 0 para la primera/SCREEN_TO_OPEN_ROOT = 1  # 1 Para la segunda pantalla y 0 para la primera/g' config.py
 
 echo "Changes pushed successfully!"
